7a77100876fd61c7b23ddd1a722bd3b552fed1b4
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 20:58:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
+from flask import Flask, jsonify
 
+from config import NOME_SISTEMA, VERSAO
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+
+    return jsonify({
+
+        "sistema": NOME_SISTEMA,
+
+        "versao": VERSAO,
+
+        "status": "online"
+
+    })
+
+
+@app.route("/teste")
+def teste():
+
+    return jsonify({
+
+        "mensagem": "BarberFlow funcionando!"
+
+    })
+
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
