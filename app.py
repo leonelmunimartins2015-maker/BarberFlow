149d973a2e217595fdebe88c57869204da66f665
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 23:44:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from config import NOME_SISTEMA, VERSAO
 from firebase import iniciar_firebase
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -87,21 +87,72 @@ def cadastrar_agendamento():
     servico = request.form["servico"]
     data = request.form["data"]
     hora = request.form["hora"]
-    duracao = request.form["duracao"]
+    duracao = int(request.form["duracao"])
 
 
     if db:
+
+        novo_inicio = datetime.strptime(
+            f"{data} {hora}",
+            "%Y-%m-%d %H:%M"
+        )
+
+        novo_fim = novo_inicio + timedelta(
+            minutes=duracao
+        )
+
+
+        existentes = db.collection("agendamentos").stream()
+
+
+        for item in existentes:
+
+            ag = item.to_dict()
+
+
+            if ag.get("data") == data:
+
+                inicio_existente = datetime.strptime(
+                    f"{ag['data']} {ag['hora']}",
+                    "%Y-%m-%d %H:%M"
+                )
+
+
+                duracao_existente = int(
+                    ag.get("duracao", 30)
+                )
+
+
+                fim_existente = inicio_existente + timedelta(
+                    minutes=duracao_existente
+                )
+
+
+                if novo_inicio < fim_existente and novo_fim > inicio_existente:
+
+                    return """
+                    <h1>❌ Horário ocupado!</h1>
+                    <p>Já existe um atendimento nesse período.</p>
+                    <a href="/agenda">Voltar</a>
+                    """
+
+
         db.collection("agendamentos").add({
+
             "nome": nome,
             "servico": servico,
             "data": data,
             "hora": hora,
             "duracao": duracao
+
         })
+
 
         mensagem = "Agendamento salvo!"
 
+
     else:
+
         mensagem = "Firebase não conectado."
 
 
@@ -112,6 +163,7 @@ def cadastrar_agendamento():
     <p>Serviço: {servico}</p>
     <p>Data: {data}</p>
     <p>Horário: {hora}</p>
+    <p>Duração: {duracao} minutos</p>
 
     <br>
 
