8399f1ddb8886126db4de5b171abc07b2c26dc7b
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 23:17:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,23 @@ def lista_clientes():
     )
 
 
+@app.route("/agenda")
+def agenda():
+
+    agendamentos = []
+
+    if db:
+        dados = db.collection("agendamentos").stream()
+
+        for agendamento in dados:
+            agendamentos.append(agendamento.to_dict())
+
+    return render_template(
+        "agenda.html",
+        agendamentos=agendamentos
+    )
+
+
 @app.route("/cadastrar_cliente", methods=["POST"])
 def cadastrar_cliente():
 
@@ -63,8 +80,50 @@ def cadastrar_cliente():
     """
 
 
+@app.route("/cadastrar_agendamento", methods=["POST"])
+def cadastrar_agendamento():
+
+    nome = request.form["nome"]
+    servico = request.form["servico"]
+    data = request.form["data"]
+    hora = request.form["hora"]
+    duracao = request.form["duracao"]
+
+
+    if db:
+        db.collection("agendamentos").add({
+            "nome": nome,
+            "servico": servico,
+            "data": data,
+            "hora": hora,
+            "duracao": duracao
+        })
+
+        mensagem = "Agendamento salvo!"
+
+    else:
+        mensagem = "Firebase não conectado."
+
+
+    return f"""
+    <h1>💈 {mensagem}</h1>
+
+    <p>Cliente: {nome}</p>
+    <p>Serviço: {servico}</p>
+    <p>Data: {data}</p>
+    <p>Horário: {hora}</p>
+
+    <br>
+
+    <a href="/agenda">
+    Voltar para agenda
+    </a>
+    """
+
+
 @app.route("/teste")
 def teste():
+
     return {
         "sistema": NOME_SISTEMA,
         "versao": VERSAO,
@@ -73,6 +132,7 @@ def teste():
 
 
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=5000
