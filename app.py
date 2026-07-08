c48cd578c5ae6ced6fc83cd081abb83501f9d7b0
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 22:15:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from config import NOME_SISTEMA, VERSAO
 
 app = Flask(__name__)
@@ -12,6 +12,21 @@ def inicio():
 @app.route("/clientes")
 def clientes():
     return render_template("clientes.html")
+
+
+@app.route("/cadastrar_cliente", methods=["POST"])
+def cadastrar_cliente():
+
+    nome = request.form["nome"]
+    telefone = request.form["telefone"]
+
+    return f"""
+    <h1>Cliente cadastrado!</h1>
+    <p>Nome: {nome}</p>
+    <p>Telefone: {telefone}</p>
+    <br>
+    <a href="/clientes">Voltar</a>
+    """
 
 
 @app.route("/teste")
